e3a294e0ad286a8ab519b8bb7b93e2573978a07b
Collin Snyder <collinsnyder97@gmail.com>
2021-10-04 18:03:47 -0600

    updated form use

--- client/script.js
+++ client/script.js
@@ -1,20 +1,29 @@
-var survey_fields = document.getElementById('sponsor_fields');
+var survey_fields = document.getElementById('right');
 var add_more_features = document.getElementById('add_more_features');
 var remove_features = document.getElementById('remove_features');
 
 add_more_features.onclick = function(){
-	var newField = document.createElement('input');
-	newField.setAttribute('type','text');
-	newField.setAttribute('name','sponser_input');
-	newField.setAttribute('class','sponser_fields');
-	newField.setAttribute('siz',50);
-	newField.setAttribute('placeholder','Feature Description');
-	survey_fields.appendChild(newField);
+	var newTitle = document.createElement('input');
+	newTitle.setAttribute('type','text');
+	newTitle.setAttribute('name','sponser_input');
+	newTitle.setAttribute('class','sponser_fields');
+	newTitle.setAttribute('size',50);
+	newTitle.setAttribute('placeholder','Feature Title');
+	survey_fields.appendChild(newTitle);
+
+	var newFeature = document.createElement('input');
+	newFeature.setAttribute('type','text');
+	newFeature.setAttribute('name','sponser_input');
+	newFeature.setAttribute('class','sponser_fields');
+	newFeature.setAttribute('size',50);
+	newFeature.setAttribute('placeholder','Feature Description');
+	survey_fields.appendChild(newFeature);
 }
 
 remove_features.onclick = function(){
 	var input_tags = survey_fields.getElementsByTagName('input');
-	if(input_tags.length > 3) {
+	if(input_tags.length > 2) {
+		survey_fields.removeChild(input_tags[(input_tags.length) - 1]);
 		survey_fields.removeChild(input_tags[(input_tags.length) - 1]);
 	}
 }
