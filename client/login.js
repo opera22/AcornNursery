0c3b2dee0c0db08591a8993b2f15095483dccf02
Collin Snyder <collinsnyder97@gmail.com>
2021-10-08 11:10:24 -0600

    Sign in page form validation completed

--- client/login.js
+++ client/login.js
@@ -1,6 +1,18 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+// FormTwo//
+const sign_email = document.getElementById('sign_email');
+const sign_password = document.getElementById('sign_password');
+//
+const form = document.getElementById('form');
+const formSecond = document.getElementById('SecondForm');
+const f_name = document.getElementById('f_name');
+const l_name = document.getElementById('l_name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+// Form //
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -10,36 +22,6 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
-function carryon() {
-	window.location.href = "userLogin.php";
-}
-
-function error() {
-	alert("There was an error with the database.  Please try again.");
-	window.location.href = "userLogin.php";
-}
-
-function navFunction() {
-	var x = document.getElementById("topNav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
-
-// FormTwo//
-// const sign_email = document.getElementById('sign_in_email');
-// const sign_password = document.getElementById('sign_in_password');
-//
-const form = document.getElementById('form');
-const f_name = document.getElementById('f_name');
-const l_name = document.getElementById('l_name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-// Form //
-
 // Form Validate
 form.addEventListener('submit', e => {
 	e.preventDefault();
@@ -47,33 +29,36 @@ form.addEventListener('submit', e => {
 	checkInputs();
 });
 
-// formTwo.addEventListener('submit', e => {
-// 	e.preventDefault();
+formSecond.addEventListener('submit', e => {
+	e.preventDefault();
 	
-// 	checkInputs();
-// });
+	SignInValidate();
+});
+
+function SignInValidate(){
+	const signInEmail = sign_email.value.trim();
+	const signInPassword = sign_password.value.trim();
+
+	if(signInEmail === '') {
+		setErrorFor(sign_email, 'Email cannot be blank');
+	} else {
+		setSuccessFor(sign_email);
+	}
+
+	if(signInPassword === '') {
+		setErrorFor(sign_password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(sign_password);
+	}
+}
 
 function checkInputs() {
-	// const signInEmail = sign_email.value.trim();
-	// const signInPassword = sign_password.value.trim();
 	// trim to remove the whitespaces
 	const firstnameValue = f_name.value.trim();
 	const lastnameValue = l_name.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
-	
-	// if(signInEmail === '') {
-	// 	setErrorFor(signInEmail, 'Email cannot be blank');
-	// } else {
-	// 	setSuccessFor(signInEmail);
-	// }
-
-	// if(signInPassword === '') {
-	// 	setErrorFor(signInPassword, 'Password cannot be blank');
-	// } else {
-	// 	setSuccessFor(signInPassword);
-	// }
 
 	if(firstnameValue === '') {
 		setErrorFor(f_name, 'First Name cannot be blank');
@@ -122,20 +107,20 @@ function setSuccessFor(input) {
 	formControl.className = 'form-control success';
 }
 	
-function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+// function isEmail(email) {
+// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+// }
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+// // SOCIAL PANEL JS
+// const floating_btn = document.querySelector('.floating-btn');
+// const close_btn = document.querySelector('.close-btn');
+// const social_panel_container = document.querySelector('.social-panel-container');
 
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
+// floating_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.toggle('visible')
+// });
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+// close_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.remove('visible')
+// });
 // Form Validate
