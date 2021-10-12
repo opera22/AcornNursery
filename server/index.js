81fff829b7ea721014f67ba22cff482afc9ca75e
DrewB <>
2021-10-12 14:06:21 -0600

    Slight changes by Drew to integrate backend

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const port = 3000;
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use( express.static( path.join( __dirname, '..', 'client')));
+// app.use(express.static(__dirname + '../client'));
 
 console.log(process.env.DB_HOST,
     process.env.DB_PORT,
