a65942cf372d70d1cc9243907bea9f204cc8db12
Collin Snyder <collinsnyder97@gmail.com>
2021-11-09 15:41:21 -0700

    adding dataTables from jQuery

---
+++ extras/datatables.js
@@ -0,0 +1,89 @@
+/* Formatting function for row details - modify as you need */
+function format(d) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Full name:</td>' +
+        '<td>' + d.name + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Extension number:</td>' +
+        '<td>' + d.extn + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Extra info:</td>' +
+        '<td>And any further details here (images etc)...</td>' +
+        '</tr>' +
+        '</table>';
+}
+
+$(document).ready(function () {
+    var table = $('#example').DataTable({
+        'ajax': 'https://gyrocode.github.io/files/jquery-datatables/objects.json',
+        'columns': [{
+                'className': 'details-control',
+                'orderable': false,
+                'data': null,
+                'defaultContent': ''
+            },
+            {
+                'data': 'name'
+            },
+            {
+                'data': 'position'
+            },
+            {
+                'data': 'office'
+            },
+            {
+                'data': 'salary'
+            }
+        ],
+        'order': [
+            [1, 'asc']
+        ]
+    });
+
+    //Controls for buttons
+    // Add event listener for opening and closing details
+    $('#example tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        } else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+
+    // Handle click on "Expand All" button
+    $('#btn-show-all-children').on('click', function () {
+        // Enumerate all rows
+        table.rows().every(function () {
+            // If row has details collapsed
+            if (!this.child.isShown()) {
+                // Open this row
+                this.child(format(this.data())).show();
+                $(this.node()).addClass('shown');
+            }
+        });
+    });
+
+    // Handle click on "Collapse All" button
+    $('#btn-hide-all-children').on('click', function () {
+        // Enumerate all rows
+        table.rows().every(function () {
+            // If row has details expanded
+            if (this.child.isShown()) {
+                // Collapse row details
+                this.child.hide();
+                $(this.node()).removeClass('shown');
+            }
+        });
+    });
+});
