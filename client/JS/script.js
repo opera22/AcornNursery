764069135aaf7b8e63b9de3b11d4b943ec6cb41c
Collin Snyder <collinsnyder97@gmail.com>
2021-11-12 15:58:20 -0700

    Simplified CSS and Added comments to pages

--- client/JS/script.js
+++ client/JS/script.js
@@ -115,7 +115,6 @@ function checkInputs() {
 		setSuccessFor(description_feature);
 	}
 
-<<<<<<< Updated upstream
 	return allGood;
 }
 
@@ -130,8 +129,3 @@ function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = "form-control success";
 }
-=======
-	const res = await axios.post("/api/projects/getall", jsonObject);
-	console.log(res.data);
-});
->>>>>>> Stashed changes
