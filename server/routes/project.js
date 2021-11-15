bee85888518944c1c244284256dedf0ace5f527e
DrewB <>
2021-11-15 15:01:17 -0700

    Features able to insert into database

--- server/routes/project.js
+++ server/routes/project.js
@@ -5,33 +5,43 @@ const db = require('../db');
 router.post('/create', (req, res) => {
 
     let project_id = null;
-
-    console.log(req.body);
-
-    try {
-        // Queries are nested because they are asynchronous and rely on each other (second query requires project_id from insert statement in first query)
-        db.query("insert into PROJECTS values (NULL, 1, ?, ?, 0, now())", [req.body.Title, req.body.Description], (err, res) => {
-            console.log("Insert ID for PROJECTS: ", res.insertId);
-            // This value needs to be used in the next query
-            project_id = res.insertId;
+    // Matching category names from client to category names in DB
+    let category = "";
+    switch (req.body.Catagory) {
+        case "web":
+            category = "Web";
+            break;
+    }
     
-            // Matching category names from client to category names in DB
-            let category = "";
-            switch (req.body.Catagory) {
-                case "web":
-                    category = "Web";
-                    break;
-            }
-    
-            db.query("insert into PROJECTS_CATEGORIES select NULL, ?, c.CATEGORY_ID from PROJECTS p cross join CATEGORIES c where c.CATEGORY_NAME = ?", [project_id, category], (err, res) => {
-                console.log("Insert ID for PROJECTS_CATEGORIES: ", res.insertId); 
+    // Queries are nested because they are asynchronous and rely on each other (second query requires project_id from insert statement in first query)
+    db.query("insert into PROJECTS values (NULL, 1, ?, ?, 0, now(), ?)", [req.body.Title, req.body.Description, category], (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-                
+        console.log("Insert ID for PROJECTS: ", res.insertId);
+        // This value needs to be used in the next query
+        project_id = res.insertId;
+
+        // The features are stored in JSON, not a list. This next line console-logs the keys and converts them to a list.
+        console.log(Object.keys(req.body.features));
+        let features_list = [];
+        Object.keys(req.body.features).forEach(feature => {
+            features_list.push(feature)
+        });
+
+        features_list.forEach(feature => {
+            db.query("insert into FEATURES values (NULL, ?, ?, ?)", [project_id, feature, req.body.features[feature]], (err, res) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log("Insert ID for feature: ", res.insertId);   
             });
         });
-    } catch (err) {
-        console.log(err);
-    }
+    });
+
     res.status(201).send(`Successfully created project ${req.body.Title}`);
 });
 
@@ -53,9 +63,9 @@ router.get('/getall?:id', (req, res) => {
                 "title": row.PROJECT_TITLE,
                 "description": row.PROJECT_DESC,
                 "date": row.PROJECT_CREATED_DATE,
-                "category": "Unknown"
+                "category": "Unknown",
+                "features": []
             });
-            console.log(responseRows.length);
         });
 
         res.send(responseRows); 
